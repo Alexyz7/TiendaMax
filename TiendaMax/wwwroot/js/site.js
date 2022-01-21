9030bf386f3ae394c26ced8018bbc3e0474dc816
Alexis Rolón <eldkno_siete@outlook.com>
2022-01-21 08:16:09 -0300

    Creacion del menu lateral

--- TiendaMax/wwwroot/js/site.js
+++ TiendaMax/wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+function abrirSide() {
+
+
+    document.getElementById("Menulateral").style.width = "250px";
+
+
+}
+
+
+function cerrarmenu() {
+
+
+    document.getElementById("Menulateral").style.width = "0";
+
+}
